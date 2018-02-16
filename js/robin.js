14125d7b569232ac7e2d1aa226c755314493378d
Olli Rissanen <olli.rissanen@aalto.fi>
2018-02-16 13:31:18 +0200

    Major fixes

---
+++ js/robin.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("nav_container").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav_container").style.width = "0%";
+}
