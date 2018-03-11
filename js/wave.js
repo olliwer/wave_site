56e08aec3d142d22454085a158765dcbc19ee749
Olli R <olliver@Ollis-MacBook-Air.local>
2018-03-11 09:41:32 +0100

    Add sumpli

--- js/wave.js
+++ js/wave.js
@@ -191,6 +191,16 @@ function openInfo_portyr() {
   }
 }
 
+function openInfo_sumpli() {
+  if (/Mobi/.test(navigator.userAgent)) {
+    document.getElementById("info_container_sumpli").style.width = "100%";
+    document.getElementById("close_container").style.width = "0%";
+  } else {
+    document.getElementById("info_container_sumpli").style.width = "45%";
+    document.getElementById("close_container").style.width = "55%";
+  }
+}
+
 
 function closeInfo() {
   document.getElementById("info_container_blok").style.width = "0%"; 
@@ -198,5 +208,6 @@ function closeInfo() {
   document.getElementById("info_container_surgify").style.width = "0%";  
   document.getElementById("info_container_quicksave").style.width = "0%";  
   document.getElementById("info_container_portyr").style.width = "0%"; 
+  document.getElementById("info_container_sumpli").style.width = "0%"; 
   document.getElementById("close_container").style.width = "0%";
 }
